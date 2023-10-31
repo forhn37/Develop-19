651a033f69425e3af861d5b7c2693c78a29f3d71
kimhyun <forhn37@gmail.com>
2023-10-31 23:01:59 +0900

    생성자 함수 테스트 완료

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
 function Person(name, age) {
-  this.name = name;
-  this.age = age;
+  this.saja = name;
+  this.horangi = age;
+  this.greeting = function () {
+    return `나는 ${this.saja}이고 ${this.horangi}입니다.`
+  }
 }
 
 const person1 = new Person('kimhyun', 37)
-console.log(person1)
+// // console.log(new Person('yoonjunhyun', '28').greeting())
+console.log(person1.greeting())
+
+// const saseum = {
+//   one : 1,
+//   two : 2,
+// }
+// console.log(saseum.one)
+
